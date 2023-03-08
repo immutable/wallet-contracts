e27db4d57ba7c2ff61279c018523494a90085071
Felipe Tavares <felipe.tavares@pragma.team>
2023-03-08 17:16:56 +1100

    adding more test cases

--- test/Walletfactory.test.ts
+++ test/Walletfactory.test.ts
@@ -47,6 +47,8 @@ describe("Wallet Factory", function () {
     };
   }
 
+  // TODO: test salt generation too?
+
   describe("getAddress", function () {
     it("Should return deterministic contract address", async function () {
       const { factory, mainModule } = await loadFixture(setupFactoryFixture);
@@ -55,6 +57,22 @@ describe("Wallet Factory", function () {
           addressOf(factory.address, mainModule.address, salt)
         );
       }
+    });
+
+    it("Should depend on all parameters and generate no collisions", async function () {
+      const { factory, mainModule, customModule } = await loadFixture(setupFactoryFixture);
+
+      // Call getAddress() with different addresses and different salts
+      const addresses = await Promise.all(
+        [mainModule.address, customModule.address].flatMap((address) =>
+          salts.map(async (salt) => factory.getAddress(address, salt))
+        )
+      );
+
+      const addressSet = new Set(addresses);
+
+      // Results should have no duplicates
+      expect(addressSet.size).to.equal(addresses.length);
     });
   });
 
@@ -139,6 +157,32 @@ describe("Wallet Factory", function () {
       await expect(factory.deploy(mainModule.address, salt)).to.be.revertedWith(
         "WalletFactory: deployment failed"
       );
+    });
+
+    it("Storage of each deployed contract should be independent (i.e.: delegatecall)", async function () {
+      const { owner, acc1, factory, customModule } = await loadFixture(
+        setupFactoryFixture
+      );
+
+      const salts = [
+        encodeImageHash(1, [{ weight: 1, address: owner.address }]),
+        encodeImageHash(1, [{ weight: 1, address: acc1.address }])
+      ];
+
+      // Deploy two contracts pointing to the same implementation
+      await Promise.all(salts.map(async salt => factory.deploy(customModule.address, salt)));
+
+      const contracts = salts.map(salt => CustomModule__factory.connect(
+        addressOf(factory.address, customModule.address, salt),
+        owner
+      ));
+
+      // Set the string stored in each contract to their address
+      await Promise.all(contracts.map(async contract => contract.setStr(contract.address)));
+
+      for (const contract of contracts) {
+        expect(await contract.getStr()).to.equal(contract.address);
+      }
     });
   });
 
