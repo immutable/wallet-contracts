eeb9c2a2b32fba9af8425006a16c730bf95c397f
Peter Robinson <drinkcoffee@eml.cc>
2023-04-14 12:32:01 +1000

    Removed unneeded compilation script

--- compileStartupWalletImpl.sh
+++
@@ -1,4 +0,0 @@
-# Copyright (c) Immutable Pty Ltd 2018 - 2023
-# Generates bytecode for StartupWalletImpl that is stored in StartupWalletFactory.sol
-solc src/contracts/startup/StartupWalletImpl.sol  --no-cbor-metadata --optimize --optimize-runs=1000 --bin
-
