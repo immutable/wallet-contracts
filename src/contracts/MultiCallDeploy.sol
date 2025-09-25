9ce53db912c91483ec0cc0a723631810014f1b84
Rodrigo Fournier <rodrigo.fournier@immutable.com>
2025-09-25 14:21:04 +1000

    Update solidity to version 0.8.27 in all contracts

--- src/contracts/MultiCallDeploy.sol
+++ src/contracts/MultiCallDeploy.sol
@@ -1,6 +1,6 @@
 // Copyright (c) Immutable Pty Ltd 2018 - 2023
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.17;
+pragma solidity 0.8.27;
 
 import "./modules/commons/interfaces/IModuleCalls.sol";
 import '@openzeppelin/contracts/access/AccessControl.sol';
