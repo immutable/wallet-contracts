b04bc29fe2468ae172ccde6822edf16458c7b251
Rodrigo Fournier <rodrigo.fournier@immutable.com>
2025-10-17 22:05:10 +1100

    Fix ModuleManager.sol

--- src/contracts/biconomy/base/ModuleManager.sol
+++ src/contracts/biconomy/base/ModuleManager.sol
@@ -51,15 +51,14 @@ abstract contract ModuleManager is Storage, EIP712, IModuleManager, RegistryAdap
   /// @notice To explicitly initialize the default validator, Nexus.execute(_DEFAULT_VALIDATOR.onInstall(...)) should be called.
   address internal immutable _DEFAULT_VALIDATOR;
 
-  // Default validator is address(0) to reuse existing validation logic in ModuleAuth
-  // /// @dev initData should block the implementation from being used as a Smart Account
-  // constructor(address defaultValidator, bytes memory initData) {
-  //     if (!IValidator(defaultValidator).isModuleType(MODULE_TYPE_VALIDATOR)) {
-  //         revert MismatchModuleTypeId();
-  //     }
-  //     IValidator(defaultValidator).onInstall(initData);
-  //     _DEFAULT_VALIDATOR = defaultValidator;
-  // }
+   /// @dev initData should block the implementation from being used as a Smart Account
+   constructor(address defaultValidator, bytes memory initData) {
+       if (!IValidator(defaultValidator).isModuleType(MODULE_TYPE_VALIDATOR)) {
+           revert MismatchModuleTypeId();
+       }
+       IValidator(defaultValidator).onInstall(initData);
+       _DEFAULT_VALIDATOR = defaultValidator;
+  }
 
   /// @notice Ensures the message sender is a registered executor module.
   modifier onlyExecutorModule() virtual {
